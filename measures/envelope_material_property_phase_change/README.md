472add2c5ba28a67b580e133b709539861549f23
Matt Steen <msteen@ambient-e.com>
2020-09-20 15:25:50 -0600

    update envelope_material_property_phase_change measure to add more than one

--- measures/envelope_material_property_phase_change/README.md
+++ measures/envelope_material_property_phase_change/README.md
@@ -5,10 +5,10 @@
 # Material Property Phase Change
 
 ## Description
-Adds a MaterialProperty:PhaseChange object to the model that references the interior layer of the selected Construction.
+
 
 ## Modeler Description
-Gets the interior layer name of the selected Construction object from the number of fields and sets the MaterialProperty:PhaseChange Name field to reference it.
+Adds a MaterialProperty:PhaseChange object to the workspace for each chosen Construction. Each MaterialProperty:PhaseChange object references the interior layer of the Construction object.
 
 ## Measure Type
 EnergyPlusMeasure
@@ -19,12 +19,84 @@ EnergyPlusMeasure
 ## Arguments
 
 
-### Construction Name
-Phase Change Material will be added to interior material
-**Name:** construction_name,
+### Construction Name 1
+A new MaterialProperty:PhaseChange object will reference the interior layer of this Construction.
+**Name:** construction_name_1,
 **Type:** Choice,
 **Units:** ,
-**Required:** true,
+**Required:** false,
+**Model Dependent:** false
+
+### Construction Name 2
+A new MaterialProperty:PhaseChange object will reference the interior layer of this Construction.
+**Name:** construction_name_2,
+**Type:** Choice,
+**Units:** ,
+**Required:** false,
+**Model Dependent:** false
+
+### Construction Name 3
+A new MaterialProperty:PhaseChange object will reference the interior layer of this Construction.
+**Name:** construction_name_3,
+**Type:** Choice,
+**Units:** ,
+**Required:** false,
+**Model Dependent:** false
+
+### Construction Name 4
+A new MaterialProperty:PhaseChange object will reference the interior layer of this Construction.
+**Name:** construction_name_4,
+**Type:** Choice,
+**Units:** ,
+**Required:** false,
+**Model Dependent:** false
+
+### Construction Name 5
+A new MaterialProperty:PhaseChange object will reference the interior layer of this Construction.
+**Name:** construction_name_5,
+**Type:** Choice,
+**Units:** ,
+**Required:** false,
+**Model Dependent:** false
+
+### Construction Name 6
+A new MaterialProperty:PhaseChange object will reference the interior layer of this Construction.
+**Name:** construction_name_6,
+**Type:** Choice,
+**Units:** ,
+**Required:** false,
+**Model Dependent:** false
+
+### Construction Name 7
+A new MaterialProperty:PhaseChange object will reference the interior layer of this Construction.
+**Name:** construction_name_7,
+**Type:** Choice,
+**Units:** ,
+**Required:** false,
+**Model Dependent:** false
+
+### Construction Name 8
+A new MaterialProperty:PhaseChange object will reference the interior layer of this Construction.
+**Name:** construction_name_8,
+**Type:** Choice,
+**Units:** ,
+**Required:** false,
+**Model Dependent:** false
+
+### Construction Name 9
+A new MaterialProperty:PhaseChange object will reference the interior layer of this Construction.
+**Name:** construction_name_9,
+**Type:** Choice,
+**Units:** ,
+**Required:** false,
+**Model Dependent:** false
+
+### Construction Name 10
+A new MaterialProperty:PhaseChange object will reference the interior layer of this Construction.
+**Name:** construction_name_10,
+**Type:** Choice,
+**Units:** ,
+**Required:** false,
 **Model Dependent:** false
 
 
--- measures/envelope_material_property_phase_change/measure.rb
+++ measures/envelope_material_property_phase_change/measure.rb
@@ -5,6 +5,10 @@
 
 # start the measure
 class EnvelopeMaterialPropertyPhaseChange < OpenStudio::Measure::EnergyPlusMeasure
+  
+  # constants
+  MAX = 10
+  
   # human readable name
   def name
     # Measure name should be the title case of the class name.
@@ -13,12 +17,12 @@ class EnvelopeMaterialPropertyPhaseChange < OpenStudio::Measure::EnergyPlusMeasu
 
   # human readable description
   def description
-    return 'Adds a MaterialProperty:PhaseChange object to the model that references the interior layer of the selected Construction.'
+    return ''
   end
 
   # human readable description of modeling approach
   def modeler_description
-    return 'Gets the interior layer name of the selected Construction object from the number of fields and sets the MaterialProperty:PhaseChange Name field to reference it.'
+    return 'Adds a MaterialProperty:PhaseChange object to the workspace for each chosen Construction. Each MaterialProperty:PhaseChange object references the interior layer of the Construction object.'
   end
 
   # define the arguments that the user will input
@@ -26,16 +30,18 @@ class EnvelopeMaterialPropertyPhaseChange < OpenStudio::Measure::EnergyPlusMeasu
     args = OpenStudio::Measure::OSArgumentVector.new
 
     # get construction names from workspace for choice argument
-    construction_names = []
+    construction_names = OpenStudio::StringVector.new
     workspace.getObjectsByType('Construction'.to_IddObjectType).sort.each do |obj|
-      construction_names << obj.getString(0).to_s
+      construction_names << obj.name.to_s
     end
     
-    # argument for construction name
-    construction_name = OpenStudio::Ruleset::OSArgument::makeChoiceArgument('construction_name', construction_names, true)
-    construction_name.setDisplayName('Construction Name')
-    construction_name.setDescription('Phase Change Material will be added to interior material')
-    args << construction_name
+    # construction name arguments
+    (1..MAX).each do |num|
+      construction_name = OpenStudio::Ruleset::OSArgument::makeChoiceArgument("construction_name_#{num}", construction_names, false)
+      construction_name.setDisplayName("Construction Name #{num}")
+      construction_name.setDescription('A new MaterialProperty:PhaseChange object will reference the interior layer of this Construction.')
+      args << construction_name
+    end
 
     return args
   end
@@ -50,33 +56,47 @@ class EnvelopeMaterialPropertyPhaseChange < OpenStudio::Measure::EnergyPlusMeasu
     end
 
     # assign the user inputs to variables
-    construction_name = runner.getStringArgumentValue('construction_name', user_arguments)
+    construction_names = []
+    (1..MAX).each do |num|
+      if runner.getOptionalStringArgumentValue("construction_name_#{num}", user_arguments).is_initialized
+        construction_names << runner.getOptionalStringArgumentValue("construction_name_#{num}", user_arguments).get
+      end
+    end
+
+    if construction_names.size == 0
+      runner.registerError('at least 1 construction name must be selected')
+      return false
+    end
 
     # reporting initial condition of model
     objs = workspace.getObjectsByType('MaterialProperty:PhaseChange'.to_IddObjectType)
     runner.registerInitialCondition("MaterialProperty:PhaseChange objects = #{objs.size}")
 
-    # get objects 
-    construction = workspace.getObjectByTypeAndName('Construction'.to_IddObjectType, construction_name).get
-    construction_num_fields = construction.numFields - 1
-    construction_interior_layer_name = construction.getString(construction_num_fields).to_s
+    # get objects
+    construction_names.each do |construction_name|
 
-    # add object from ~/EnergyPlus-x-x-x/ExampleFiles/MaterialPropertyPhaseChange.idf 
-    idf_str = "
-      MaterialProperty:PhaseChange,
-        #{construction_interior_layer_name},  !- Name
-        0,                       !- Temperature Coefficient for Thermal Conductivity {W/m-K2}
-        -20,                     !- Temperature 1 {C}
-        0.1,                     !- Enthalpy 1 {J/kg}
-        22,                      !- Temperature 2 {C}
-        18260,                   !- Enthalpy 2 {J/kg}
-        22.1,                    !- Temperature 3 {C}
-        32000,                   !- Enthalpy 3 {J/kg}
-        60,                      !- Temperature 4 {C}
-        71000;                   !- Enthalpy 4 {J/kg}
-      "
-      idf_obj = OpenStudio::IdfObject.load(idf_str).get
-      workspace.addObject(idf_obj)
+      construction = workspace.getObjectByTypeAndName('Construction'.to_IddObjectType, construction_name).get
+      construction_num_fields = construction.numFields - 1
+      construction_interior_layer_name = construction.getString(construction_num_fields).to_s
+
+      # add object from ~/EnergyPlus-x-x-x/ExampleFiles/MaterialPropertyPhaseChange.idf 
+      idf_str = "
+        MaterialProperty:PhaseChange,
+          #{construction_interior_layer_name},  !- Name
+          0,                       !- Temperature Coefficient for Thermal Conductivity {W/m-K2}
+          -20,                     !- Temperature 1 {C}
+          0.1,                     !- Enthalpy 1 {J/kg}
+          22,                      !- Temperature 2 {C}
+          18260,                   !- Enthalpy 2 {J/kg}
+          22.1,                    !- Temperature 3 {C}
+          32000,                   !- Enthalpy 3 {J/kg}
+          60,                      !- Temperature 4 {C}
+          71000;                   !- Enthalpy 4 {J/kg}
+        "
+        idf_obj = OpenStudio::IdfObject.load(idf_str).get
+        workspace.addObject(idf_obj)
+
+    end
 
     # report final condition of model
     objs = workspace.getObjectsByType('MaterialProperty:PhaseChange'.to_IddObjectType)
